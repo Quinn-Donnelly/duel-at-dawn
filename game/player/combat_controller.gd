9699132bb2359e143b0f6b16c0b7c9b02185cb38
Quinn Donnelly <quinndonnelly22@gmail.com>
2025-11-23 16:53:29 -0500

    feat: adding punch animation

---
+++ game/player/combat_controller.gd
@@ -0,0 +1,19 @@
+class_name CombatController
+extends Node
+
+@export var hitBox: HitBox
+@export var audioPlayer: AudioStreamPlayer2D
+@export var animationPlayer: AnimationPlayer
+
+func _ready() -> void:
+	hitBox.hit.connect(self._on_hit)
+
+func _unhandled_input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("punch"):
+		_punch()
+
+func _on_hit() -> void:
+	audioPlayer.play()
+
+func _punch() -> void:
+	animationPlayer.play("punch")
--- game/player/player.gd
+++ game/player/player.gd
@@ -1,11 +1,2 @@
 class_name Player
 extends CharacterBody2D
-
-@onready var hitBox: HitBox = $HitBox
-@onready var audioPlayer: AudioStreamPlayer2D = $HitAudio
-
-func _ready() -> void:
-	hitBox.hit.connect(self._on_hit)
-	
-func _on_hit() -> void:
-	audioPlayer.play()
