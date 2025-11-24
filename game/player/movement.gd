40257ebbd843b44f6657d1058014d6c64015a5b6
Quinn Donnelly <quinndonnelly22@gmail.com>
2025-11-24 15:48:19 -0500

    feat: walking animation
    - the ugliest one i've seen but it's there

--- game/player/movement.gd
+++ game/player/movement.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 ## Speed at which player will move
 @export var movement_speed: float = 100
+@export var animationPlayer: AnimationPlayer
 
 func _ready() -> void:
 	owner.set_meta(Constants.MOVEMENT_COMPONENT, self)
@@ -10,6 +11,8 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var direction = Input.get_axis("move_left", "move_right")
 	owner.global_position.x = owner.global_position.x + movement_speed * direction * delta
+	if direction and animationPlayer:
+		animationPlayer.play("walk")
 
 func _exit_tree() -> void:
 	owner.remove_meta(Constants.MOVEMENT_COMPONENT)
