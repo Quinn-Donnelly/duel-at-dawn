eb362b037c82af8df33d85a3f8dcbaea6a4d843c
Quinn Donnelly <quinndonnelly22@gmail.com>
2025-11-25 11:04:28 -0500

    feat: tuning punch and walk animations

--- game/player/movement.gd
+++ game/player/movement.gd
@@ -3,16 +3,13 @@ extends Node2D
 
 ## Speed at which player will move
 @export var movement_speed: float = 100
-@export var animationPlayer: AnimationPlayer
 
 func _ready() -> void:
 	owner.set_meta(Constants.MOVEMENT_COMPONENT, self)
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	var direction = Input.get_axis("move_left", "move_right")
-	owner.global_position.x = owner.global_position.x + movement_speed * direction * delta
-	if direction and animationPlayer:
-		animationPlayer.play("walk")
-
+	owner.velocity.x = movement_speed * direction
+	
 func _exit_tree() -> void:
 	owner.remove_meta(Constants.MOVEMENT_COMPONENT)
--- game/player/player.gd
+++ game/player/player.gd
@@ -2,8 +2,13 @@ class_name Player
 extends CharacterBody2D
 
 @onready var fighter: Fighter = $Fighter
+@onready var animationPlayer: AnimationPlayer = $AnimationPlayer
 
 func _process(_delta: float) -> void:
+	if velocity and animationPlayer.current_animation == "idle":
+		animationPlayer.play("walk")
+	if animationPlayer.current_animation == "walk" and not velocity:
+		animationPlayer.play("idle")
 	move_and_slide()
 
 func get_fighter() -> Fighter:
