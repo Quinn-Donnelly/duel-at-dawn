0ca717b32a2f594e18bb06958cee613f687ce064
Quinn Donnelly <quinndonnelly22@gmail.com>
2025-12-01 07:12:29 -0500

    feat: adding randomized reaction delay

--- game/ai/combat_state_machine/attack_ai_state.gd
+++ game/ai/combat_state_machine/attack_ai_state.gd
@@ -3,9 +3,10 @@ extends State
 
 @export var punch_cooldown: Timer
 @export var animation_player: AnimationPlayer
-
+@export var attackDelay: Curve
 
 var state_machine: AiCombatStateMachine
+var isPunching: bool = false
 
 func _ready() -> void:	
 	state_machine = get_parent() as AiCombatStateMachine
@@ -17,14 +18,21 @@ func enter() -> void:
 	animation_player.animation_finished.connect(self._on_animation_finished)
 	
 func process(_delta: float) -> void:
-	if punch_cooldown.is_stopped() and animation_player.current_animation != "punch":
+	if punch_cooldown.is_stopped() and not isPunching:
 		# for now having cooldown at start to avoid potential race
-		punch_cooldown.start()
-		animation_player.play("punch")
+		isPunching = true
+		var delay = attackDelay.sample(randf())
+		var timer = get_tree().create_timer(delay)
+		timer.timeout.connect(_do_punch)
 
 func exit() -> void:
 	animation_player.animation_finished.disconnect(self._on_animation_finished)
 
+func _do_punch() -> void:
+	punch_cooldown.start()
+	animation_player.play("punch")
+
 func _on_animation_finished(animation_name: String) -> void:
 	if animation_name == "punch":
+		isPunching = false
 		state_machine.change_state("Idle")
