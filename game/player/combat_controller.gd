e550973429f28f5501736a62ee1a948527633edc
Quinn Donnelly <quinndonnelly22@gmail.com>
2025-12-03 14:42:53 -0500

    feat: very basic combo system
    - likely needs more consistent timing tuning, _unhandled input might not
      be the loop to be in for a good feel

--- game/player/combat_controller.gd
+++ game/player/combat_controller.gd
@@ -5,6 +5,10 @@ extends Node
 @export var audioPlayer: AudioStreamPlayer2D
 @export var animationPlayer: AnimationPlayer
 
+@export var comboWindow: bool = false
+var wantsCombo: bool = false
+var isPunching: bool = false
+
 func _ready() -> void:
 	assert(animationPlayer, "CombatController must have an animation Player")
 	assert(audioPlayer, "CombatController must have an audio player")
@@ -13,7 +17,11 @@ func _ready() -> void:
 
 func _unhandled_input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("punch"):
-		_punch()
+		if not isPunching:
+			_punch()
+		
+		if comboWindow:
+			wantsCombo = true
 	if Input.is_action_just_pressed("uppercut"):
 		_uppercut()
 
@@ -21,7 +29,21 @@ func _on_hit() -> void:
 	audioPlayer.play()
 
 func _punch() -> void:
+	isPunching = true
 	animationPlayer.play("jab")
 
 func _uppercut() -> void:
 	animationPlayer.play("uppercut")
+
+func _on_punch_finished() -> void:
+	if wantsCombo:
+		animationPlayer.play("uppercut")
+		return
+	
+	isPunching = false
+	animationPlayer.play("idle")
+	
+func _on_uppercut_finished() -> void:
+	isPunching = false
+	wantsCombo = false
+	animationPlayer.play("idle")
